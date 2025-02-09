171205c3390476b4963d5252585c69d37aaf4986
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2025-02-09 16:04:29 +0000

    Create local DB for contacts

--- contacts.py
+++ contacts.py
@@ -1,62 +1,73 @@
+import json
+
 class Contact:
     name: str
-    phone: int
+    age: int
+    phone: str
 
-    def __init__(self, name: str, phone: int) -> None:
+    def __init__(self, name: str, age: int, phone: str) -> None:
         self.name = name
+        self.age = age
         self.phone = phone
 
     def __str__(self) -> str:
-        return f"Name: {self.name}, Phone: {self.phone}"
+        return f"{self.name}, {self.age}y, {self.phone}"
 
 class Book:
     contacts: list[Contact]
 
     def __init__(self):
         self.contacts = []
+        file = open('./contacts.json', 'r')
+        content = file.read()
+        file.close()
+        saved_contacts = json.loads(content)
+        for new_contact in saved_contacts:
+            self.add(Contact(new_contact["name"], new_contact["age"], new_contact["phone"]))
 
-    def add_contact(self, *contacts: Contact) -> None:
-        for contact in contacts:
-            self.contacts.append(contact)
+    def add(self, contact: Contact) -> None:
+        self.contacts.append(contact)
 
-    def remove_contacts(self, phone: int) -> list[Contact]:
-        contacts = [contact for contact in self.contacts if contact.phone == phone]
-        for contact in contacts:
+    def remove(self, phone: str) -> list[Contact]:
+        removing_contacts = [contact for contact in self.contacts if contact.phone == phone]
+        for contact in removing_contacts:
             self.contacts.remove(contact)
-        return contacts
+        return removing_contacts
 
-    def view_contacts(self, phone: int) -> list[str]:
-        contacts = [str(contact) for contact in self.contacts if contact.phone == phone]
-        return contacts
+    def save(self) -> None:
+        contacts_dictionaries = [contact.__dict__ for contact in self.contacts]
+        json_dumps = json.dumps(contacts_dictionaries)
+        file = open('./contacts.json', 'w')
+        file.write(json_dumps)
+        file.close()
 
 book = Book()
-
 while True:
-    response = input().split(" ")
-    method = response[0]
-    arguments = "".join(response[1:])
-    if method not in ["add", "list", "remove", "view"]:
-        print("The only methods avaiable are 'add', 'list', 'remove' and 'view'")
-    if method == "add":
-        if not arguments:
-            contact = input("Type the new contact's name and phone number separated by a ',': ")
-        else:
-            contact = arguments
-        name = str(contact.split(",")[0])
-        phone = int(contact.split(",")[1])
-        book.add_contact(Contact(name, phone))
-        print("Contact added successfully")
-    if method == "list":
-        contacts = [contact for contact in book.contacts]
-        print("----------")
-        for contact in contacts:
-            print(contact)
-        print("----------")
-    if method == "remove":
-        phone = int(input("Type the contact phone number: "))
-        removed_contacts = book.remove_contacts(phone)
-        print(f"The following contacts were removed: {", ".join([contact.name for contact in removed_contacts])}")
-    if method == "view":
-        phone = int(input("Type the contact phone number: "))
-        contacts = book.view_contacts(phone)
-        print(f"The following contacts has this phone number: [{"] and [".join([contact for contact in contacts])}]")
+    method = input("Type 'add(a)', 'list(l)', 'remove(r)' or 'quit(q)': ")
+    if method not in ["add", "a",  "list", "l",  "remove", "r",  "quit", "q"]:
+        print("The only methods avaiable are 'add(a)', 'list(l)', 'remove(r)' or 'quit(q)'")
+    else:
+        if method == "add" or method == "a":
+            print("--------------------")
+            name = input("Name: ")
+            age = int(input("Age: "))
+            phone = input("Phone number: ")
+            book.add(Contact(name, age, phone))
+            book.save()
+            print("Contact added successfully")
+            print("--------------------")
+        if method == "list" or method == "l":
+            print("--------------------")
+            contacts = [contact for contact in book.contacts]
+            for contact in contacts:
+                print(contact)
+            print("--------------------")
+        if method == "remove" or method == "r":
+            print("--------------------")
+            phone = input("Type the contact phone number: ")
+            removed_contacts = book.remove(phone)
+            book.save()
+            print(f"The following contacts were removed: {", ".join([contact.name for contact in removed_contacts])}")
+            print("--------------------")
+        if method == "quit" or method == "q":
+            break
